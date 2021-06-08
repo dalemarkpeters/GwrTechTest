dd7d8f66e61629167d5cc3f73000456ba623c6bd
Dale Mark Peters <dmp9@aber.ac.uk>
2021-06-08 18:39:46 +0100

    Refactored common code in tests

--- ShapeProcessorTests/UnitTest1.cs
+++ ShapeProcessorTests/UnitTest1.cs
@@ -5,34 +5,16 @@ namespace ShapeProcessorTests
     [TestFixture]
     public class UnitTest1
     {
-        [Test]
-        public void CanCalculatePerimeterOfAShapeGivenItsSideLengthAndNumberOfSides()
+        [TestCase(6, 2, 12)]
+        [TestCase(8, 8, 64)]
+        [TestCase(1, 4, 4)]
+        public void CanCalculatePerimeterOfAShapeGivenItsSideLengthAndNumberOfSides(int numSides, int sideLength, int expectedResult)
         {
             var perimeterCalculator = new PerimeterCalculator();
 
-            var result = perimeterCalculator.CalculatePerimeter(numberOfSides: 6, sideLength: 2);
+            var result = perimeterCalculator.CalculatePerimeter(numSides, sideLength);
 
-            Assert.AreEqual(12, result);
-        }
-
-        [Test]
-        public void CanCalculatePerimeterOfShapeGiven8SidesOfLength8cm()
-        {
-            var perimeterCalculator = new PerimeterCalculator();
-
-            var result = perimeterCalculator.CalculatePerimeter(numberOfSides: 8, sideLength: 8);
-
-            Assert.AreEqual(64, result);
-        }
-
-        [Test]
-        public void CanCalculatePerimeterOfShapeGiven1SideOf4cm()
-        {
-            var perimeterCalculator = new PerimeterCalculator();
-
-            var result = perimeterCalculator.CalculatePerimeter(numberOfSides: 1, sideLength: 4);
-
-            Assert.AreEqual(4, result);
+            Assert.AreEqual(expectedResult, result);
         }
     }
 }
