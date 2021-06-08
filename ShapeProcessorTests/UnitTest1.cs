a97841df97a71bb3b4d69a3ab7198788be1ac896
Dale Mark Peters <dmp9@aber.ac.uk>
2021-06-08 18:37:15 +0100

    Add another test

--- ShapeProcessorTests/UnitTest1.cs
+++ ShapeProcessorTests/UnitTest1.cs
@@ -24,5 +24,15 @@ namespace ShapeProcessorTests
 
             Assert.AreEqual(64, result);
         }
+
+        [Test]
+        public void CanCalculatePerimeterOfShapeGiven1SideOf4cm()
+        {
+            var perimeterCalculator = new PerimeterCalculator();
+
+            var result = perimeterCalculator.CalculatePerimeter(numberOfSides: 1, sideLength: 4);
+
+            Assert.AreEqual(4, result);
+        }
     }
 }
