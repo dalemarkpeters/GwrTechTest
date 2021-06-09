d0db99a22ecc3a7e8f8a2a08e5b842814d27c99e
Dale Mark Peters <dmp9@aber.ac.uk>
2021-06-09 19:51:37 +0100

    Console app

--- ShapeProcessor/Program.cs
+++ ShapeProcessor/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using ShapesProcessor.Input;
+using System;
+using System.IO;
+using System.Linq;
 
 namespace ShapeProcessor
 {
@@ -6,7 +9,29 @@ namespace ShapeProcessor
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var headerIsIncluded = false;
+            string filePath;
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Welcome to the Shape Processor command line interface!");
+                Console.WriteLine("Please enter the name of the CSV file you wish to be processed:");
+                filePath = Console.ReadLine();
+
+                Console.WriteLine("Does the CSV file have a header? (Y/N)");
+                var headerIncludedString = Console.ReadLine();
+                if (headerIncludedString.ToLowerInvariant() == "y".ToLowerInvariant())
+                {
+                    headerIsIncluded = true;
+                }
+            }
+            else
+            {
+                filePath = args[0];
+                headerIsIncluded = true;
+            }
+
+            var shapesReader = new ShapesReader();
+            var shapes = shapesReader.Read(filePath, headerIsIncluded);
         }
     }
 }
