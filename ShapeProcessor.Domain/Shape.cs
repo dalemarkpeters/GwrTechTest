114661a40f26740d728255ed62d5a3083c53cf3b
Dale Mark Peters <dmp9@aber.ac.uk>
2021-06-09 19:14:03 +0100

    Make test pass

--- ShapeProcessor.Domain/Shape.cs
+++ ShapeProcessor.Domain/Shape.cs
@@ -1,11 +1,24 @@
-﻿namespace ShapeProcessor.Domain
+﻿using System;
+
+namespace ShapeProcessor.Domain
 {
     public class Shape
     {
         public int NumberOfSides { get; set; }
 
         public int SideLengthInCentimetres { get; set; }
-        public PerimeterCategory PerimeterCategory { get; }
+        public PerimeterCategory PerimeterCategory { get { return GetPerimeterCategory(); }}
+
+        private PerimeterCategory GetPerimeterCategory()
+        {
+            switch (CalculatePerimeter())
+            {
+                case int p when p >= 10 && p < 19:
+                    return PerimeterCategory.Between10cmAnd19cm;
+                default:
+                    return PerimeterCategory.Under10cm;
+            };
+        }
 
         public Shape(int numberOfSides, int sideLengthInCentimetres)
         {
