37649e813c8499bb42bd751180a1d894f2296ff7
Dale Mark Peters <dmp9@aber.ac.uk>
2021-06-10 21:52:47 +0100

    Add database files

---
+++ Database/dbo.PerimeterCategories.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[PerimeterCategories]
+(
+	[Id] INT NOT NULL PRIMARY KEY,
+	[Description] NVARCHAR(100) NOT NULL
+)
---
+++ Database/dbo.Shapes.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Shapes]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [NumberOfSides] INT NOT NULL, 
+    [SideLengthInCentimetres] INT NOT NULL
+)
